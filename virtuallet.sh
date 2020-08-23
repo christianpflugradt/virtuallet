2887a68aa3e21a57700e82a0575c086761add310
Christian Pflugradt <christian@pflugradts.de>
2020-08-23 16:19:19 +0200

    implemented C edition

--- virtuallet.sh
+++ virtuallet.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
 RND=$$
-RANGE=2
-VARIANT=$(($(($RND%$RANGE))+1))
+EDITIONS_COUNT=3
+SELECTED_EDITION=$(($(($RND%$EDITIONS_COUNT))+1))
 
-if [ $VARIANT == 1 ]; then
+if [ $SELECTED_EDITION == 1 ]; then
   cd python
   python3 virtuallet.py
-elif [ $VARIANT == 2 ]; then
+elif [ $SELECTED_EDITION == 2 ]; then
   cd java
   javac -cp sqlite-jdbc.jar:. virtuallet.java
   java -cp sqlite-jdbc.jar:. virtuallet
+elif [ $SELECTED_EDITION == 3 ]; then
+  cd c
+  gcc -std=gnu89 virtuallet.c -o virtuallet.out -lsqlite3 -lm
+  ./virtuallet.out
 fi
